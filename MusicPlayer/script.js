75b1714adbcf69a2b8b1511a2514aa2cbfcb127d
Dyddds <ndooods@gmail.com>
2024-04-04 08:36:08 -0500

    continuation

--- MusicPlayer/script.js
+++ MusicPlayer/script.js
@@ -126,6 +126,16 @@ const playPreviousSong = () =>{
     playSong(previousSong.id);
     }
 };
+
+const highlightCurrentSong = () => {
+    const playlistSongElements = document.querySelectorAll(".playlist-song");
+    const songToHighlight = document.getElementById(`song-${userData?.currentSong?.id}`);
+    playlistSongElements.forEach((songEl)=>{
+        songEl.removeAttribute("aria-current");
+        if(songToHighlight){songToHighlight.setAttribute("aria-current","true");}
+        
+    });
+};
 const renderSongs = (array) => {
   const songsHTML = array
     .map((song)=> {
@@ -159,6 +169,7 @@ playButton.addEventListener("click",()=>{
 
 pauseButton.addEventListener("click",pauseSong);
 nextButton.addEventListener("click",playNextSong);
+previousButton.addEventListener("click",playPreviousSong);
 // ?. returns null if inaccessible
 // sort accepts -1 from callback function puts a before b
 const sortSongs = () => {
